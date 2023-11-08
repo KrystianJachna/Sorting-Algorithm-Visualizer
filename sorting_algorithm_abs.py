3fdd09b5457b7edcfa926ee014d0b5afb7a86670
Krystian Jachna <krysti4nape@gmail.com>
2023-11-08 22:56:43 +0100

    final version

--- sorting_algorithm_abs.py
+++ sorting_algorithm_abs.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 
 
 class SortingAlgorithm(ABC):
+
     @staticmethod
     @abstractmethod
     def sort(lst: list[int]) -> GeneratorExit(tuple[tuple[int, int], tuple[int, int]]):
@@ -14,5 +15,3 @@ class SortingAlgorithm(ABC):
         :param lst: list of values to be sorted
         :return: tuple of indexes where each index is a value that is being compared
         """
-
-    #TODO: make functions compare/swap to show progress instead of yielding to make it more friendly to upgrade
